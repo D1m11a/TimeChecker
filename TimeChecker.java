219f56f8a6780e24f1abdd4daa75890b9fb83405
D1m11a <128309725+D1m11a@users.noreply.github.com>
2023-08-19 15:17:29 +0300

    Add files via upload

---
+++ TimeChecker.java
@@ -0,0 +1,35 @@
+package TimeChecker;
+
+public class TimeChecker {
+    public static void main(String[] args) {
+        Thread clockThread = new Thread(() -> {
+            long startTime = System.currentTimeMillis();
+            while (true) {
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = (currentTime - startTime) / 1000;
+                System.out.println("Пройшло " + elapsedTime + " секунд");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread messageThread = new Thread(() -> {
+            while (true) {
+                System.out.println("Минуло 5 секунд");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        clockThread.start();
+        messageThread.start();
+    }
+}
+
